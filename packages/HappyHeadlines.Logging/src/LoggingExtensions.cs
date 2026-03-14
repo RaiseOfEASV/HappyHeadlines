27a818d7737f206f8e9a7566ccfadf87a26217c1
Ju5tAmir <amirbakhshizad53@gmail.com>
2026-03-14 19:12:04 +0100

    chore: created shared package for logging and defined the configurations

---
+++ packages/HappyHeadlines.Logging/src/LoggingExtensions.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Builder;
+using Serilog;
+using Serilog.Formatting.Compact;
+
+namespace HappyHeadlines.Logging;
+
+public static class LoggingExtensions
+{
+    public static WebApplicationBuilder AddServiceLogging(
+        this WebApplicationBuilder builder,
+        string serviceName)
+    {
+        builder.Host.UseSerilog((context, services, configuration) =>
+        {
+            var loggerConfig = configuration
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.WithEnvironmentName()
+                .Enrich.WithProperty("ServiceName", serviceName)
+                .WriteTo.Console(new CompactJsonFormatter());
+
+            var seqUrl = context.Configuration["Seq:ServerUrl"];
+            if (!string.IsNullOrEmpty(seqUrl))
+            {
+                var apiKey = context.Configuration["Seq:ApiKey"];
+                loggerConfig.WriteTo.Seq(seqUrl, apiKey: string.IsNullOrEmpty(apiKey) ? null : apiKey);
+            }
+        });
+
+        return builder;
+    }
+
+    public static WebApplication UseServiceLogging(this WebApplication app)
+    {
+        app.UseSerilogRequestLogging(options =>
+        {
+            options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+            {
+                diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value ?? string.Empty);
+                diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
+            };
+        });
+
+        return app;
+    }
+}
